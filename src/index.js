cd61cb9280c1746b99da460cbde1e6564867d2a1
nator <gowluonade@gmail.com>
2024-05-21 23:02:28 -0500

    f

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require("whatsapp-web.js")
+const qrcode = require("qrcode-terminal")
+const { MessageMedia } = require("whatsapp-web.js")
 
-const client = new Client();
+const wwebVersion = '2.2412.54';
+const id = "0001"
+const clients = {}
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+clients[id] = new Client({
+    authStrategy: new LocalAuth({
+        clientId: "0001"
+    }),
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-gpu'],
+    },
+    webVersionCache: {
+        type: 'remote',
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+    }
+})
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
+clients[id].initialize().catch(err => console.log(err))
 
-client.on('*', any => { console.log(any) });
+clients[id].on("qr", (qr) => {
+    console.log(qr)
+    qrcode.generate(qr, { small: true })
+})
+clients[id].on("ready", () => console.log("Client is ready!"))
 
-client.initialize();
+clients[id].on("message", async (msg) => {
+    try {
+        if (process.env.PROCCESS_MESSAGE_FROM_CLIENT && msg.from != "status@broadcast") {
+            const contact = await msg.getContact()
+            console.log(contact, msg.from)
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
